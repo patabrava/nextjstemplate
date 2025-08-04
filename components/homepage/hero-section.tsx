89bcf540fbf938a8bf515119e4c785e1b27a989d
Frederickzoller <caposk8@hotmail.com>
2025-08-05 01:10:52 +0200

    returned hero section to default

--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -7,31 +7,50 @@ export default function HeroSection() {
     <section className="py-20">
       <div className="relative z-10 mx-auto w-full max-w-2xl px-6 lg:px-0">
         <div className="relative text-center">
-          <p className="text-3xl">✨</p>
+          <p className="text-3xl">🔥</p>
           <h1 className="mx-auto mt-12 max-w-xl text-balance text-5xl font-medium">
-            Your Personal AI Skin Coach
+            Nextjs Starter Kit
           </h1>
           <p className="text-muted-foreground mx-auto mb-6 mt-4 text-balance text-xl">
-            Get a personalized skin analysis and routine, powered by AI. We
-            respect your privacy and will never share your data.
+            This powerful starter kit is designed to help you launch your SAAS
+            application quickly and efficiently.
           </p>
           <div className="flex flex-col items-center gap-2 *:w-full sm:flex-row sm:justify-center sm:*:w-auto">
             <Button asChild variant="default" size="sm">
-              <Link href="/sign-up" prefetch={true}>
-                <span className="text-nowrap">Get Your Free Analysis</span>
+              <Link href="/dashboard" prefetch={true}>
+                <span className="text-nowrap">Get Started</span>
+              </Link>
+            </Button>
+            <Button asChild variant="outline" size="sm">
+              <Link
+                href="https://github.com/michaelshimeles/nextjs-starter-kit"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="text-nowrap">Github</span>
               </Link>
             </Button>
           </div>
         </div>
 
-        <div className="relative mt-8 overflow-hidden rounded-3xl">
+        <div className="relative mt-8 overflow-hidden rounded-3xl bg-black/10">
           <Image
-            src="/iphone.png"
-            alt="iPhone with app screenshot"
-            className="relative mx-auto"
-            width={300}
-            height={600}
+            src="https://images.unsplash.com/photo-1547623641-d2c56c03e2a7?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className="absolute inset-0 size-full object-cover"
+            width={1920}
+            height={1080}
           />
+
+          <div className="bg-background rounded-(--radius) relative m-4 overflow-hidden border border-transparent shadow-xl shadow-black/15 ring-1 ring-black/10 sm:m-8 md:m-12">
+            <Image
+              src="https://jdj14ctwppwprnqu.public.blob.vercel-storage.com/GsZRNq5WsAAMbrG-H9YrPK4HJnXSQV692jECFST4zyYpva.jpg"
+              alt="app screen"
+              width="2880"
+              height="1842"
+              className="object-top-left size-full object-cover"
+            />
+          </div>
         </div>
       </div>
     </section>
